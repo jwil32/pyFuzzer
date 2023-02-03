73ec635f018f867c040859068254fbe3d2717f36
Jonathan Wilson <68436526+jwil32@users.noreply.github.com>
2023-02-03 08:11:01 -0700

    Started adding functions

--- fuzzer.py
+++ fuzzer.py
@@ -1,5 +1,5 @@
 # Created: 1/30/2023 by Jonathan Wilson
-# Updated: 
+# Version: 0.0.2
 # 
 # 
 # Arguments
@@ -13,72 +13,79 @@ from os.path import exists
 from termcolor import colored
 import re
 import requests
-import resource
-import os
-import encodings
+
+def FileExists(_file):
+    fileExists = exists(_file)
+    return fileExists
+
+def validateURL(_url):
+    isValid = re.match('^(http(s){0,1}:\/\/){0,1}(([a-zA-Z]*\.){0,1}([a-zA-Z0-9]*)\.[a-zA-Z]{1,10}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(\/[\d\w\s\/\.\-\+\_\?\=\|\$\^\\\(\)\{\}\[\]]*){0,1}$', _url)
+    return isValid
 
 
-print("FUZZER")
-print("Usage: fuzzer.py <Target URL> <Port> <Wordlist>")
-print("")
+if __name__ == "__main__":
 
-if len(sys.argv) !=  4:
-    print("Not enough arguments")
-    exit()
+    print("FUZZER")
+    print("Usage: fuzzer.py <Target URL> <Port> <Wordlist>")
+    print("")
 
-url = sys.argv[1]
-port = int(sys.argv[2])
-wordlist = sys.argv[3]
-error = 0
+    if len(sys.argv) !=  4:
+        print("Not enough arguments")
+        exit()
 
-# Validate the URL
-validURL = re.match('^(http(s){0,1}:\/\/){0,1}(([a-zA-Z]*\.){0,1}([a-zA-Z0-9]*)\.[a-zA-Z]{1,10}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(\/[\d\w\s\/\.\-\+\_\?\=\|\$\^\\\(\)\{\}\[\]]*){0,1}$', url)
-if validURL:
-    print("Target: \t" + url)
-else:
-    print(colored("Target: Not a valid URL or IP address! (%s)" % url, 'red'))
-    error = 1
+    url = sys.argv[1]
+    port = int(sys.argv[2])
+    wordlist = sys.argv[3]
+    error = 0
 
-# Validate the port
-if port > 65535:
-    print(colored("Port: Not a valid port! (%d)" % port, 'red'))
-    error = 1
-else:
-    print("Port: \t\t%d" % port)
+    # Validate the URL
+    isValidURL = validateURL(url)
 
-# Validate that the wordlist exists
-fileExists = exists(wordlist)
-if not fileExists:
-    print(colored("Wordlist: Not a valid file path! (%s)" % wordlist, 'red'))
-    error = 1
-else:
-    print("Wordlist: \t" + wordlist)
+    if isValidURL:
+        print("Target: \t" + url)
+    else:
+        print(colored("Target: \tNot a valid URL or IP address! (%s)" % url, 'red'))
 
+    # Validate the port
+    if port > 65535:
+        print(colored("Port: Not a valid port! (%d)" % port, 'red'))
+        error = 1
+    else:
+        print("Port: \t\t%d" % port)
 
-if error:
-    exit()
+    # Validate that the wordlist exists
+    wlExists = FileExists(wordlist)
+    if not wlExists:
+        print(colored("Wordlist: Not a valid file path! (%s)" % wordlist, 'red'))
+        error = 1
+    else:
+        print("Wordlist: \t" + wordlist)
 
 
-# Build the proper URL. Needs http:// if it doesn't have it
-hasHTTP = re.match('^(http(s){0,1}:\/\/){1}', url)
-
-if not hasHTTP:
-    url = "http://" + url
+    if error:
+        exit()
 
 
-print("Effective URL:\t%s\n" % url)
+    # Build the proper URL. Needs http:// if it doesn't have it
+    hasHTTP = re.match('^(http(s){0,1}:\/\/){1}', url)
 
-# Open the wordlist
-wordlistOpen = open(wordlist, 'r')
+    if not hasHTTP:
+        url = "http://" + url
 
+    # print("Effective URL:\t%s\n" % url)
 
-for line in wordlistOpen:
-    #print(line)
-    urlFuzz = url + "/"+ line.replace("\n", "") + "/"
-    r = requests.get(urlFuzz)
-    if r.status_code == 200:
-        print(colored("[*] ", 'blue') + urlFuzz + " " + str(r.status_code))
+    
+    # # Open the wordlist
+    wordlistOpen = open(wordlist, 'r')
 
-wordlistOpen.close()
+    print("")
+
+    for line in wordlistOpen:
+        urlFuzz = url + "/"+ line.replace("\n", "") + "/"
+        r = requests.get(urlFuzz)
+        if r.status_code == 200:
+            print(colored("[*] ", 'blue') + urlFuzz + " " + str(r.status_code))
+
+    wordlistOpen.close()
 
 
