80c43b6ff1b220f11d2a1f6066c0e1a77bb77b08
Jonathan Wilson <68436526+jwil32@users.noreply.github.com>
2023-02-06 10:23:10 -0700

    Param rework, run check
    Removed the port parameter and updated the URL validation regex to allow port designation.
    Added a confirmation check before starting the fuzzing

--- fuzzer.py
+++ fuzzer.py
@@ -4,9 +4,12 @@
 # 
 # Arguments
 # 1: Target URL
-# 2: Port
-# 3: Wordlist
-
+# 2: Wordlist
+#
+# Todo:
+# 1. Add option to specify if you want to search for directories, files, or both. Allow multiple file endings
+# 2. Calculate the total amount of requests that are going to be run and display it
+# 3. Progress bar?
 
 import sys
 from os.path import exists
@@ -18,40 +21,32 @@ def FileExists(_file):
     fileExists = exists(_file)
     return fileExists
 
-def validateURL(_url):
-    isValid = re.match('^(http(s){0,1}:\/\/){0,1}(([a-zA-Z]*\.){0,1}([a-zA-Z0-9]*)\.[a-zA-Z]{1,10}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(\/[\d\w\s\/\.\-\+\_\?\=\|\$\^\\\(\)\{\}\[\]]*){0,1}$', _url)
+def ValidateURL(_url):
+    isValid = re.match('^(http(s)?:\/\/)?(([a-zA-Z]*\.)?([a-zA-Z0-9]*)\.[a-zA-Z]{1,10}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(\:[0-9]{1,5})?(\/[\d\w\s\/\.\-\+\_\?\=\|\$\^\\\(\)\{\}\[\]]*)?$', _url)
     return isValid
 
 
 if __name__ == "__main__":
 
     print("FUZZER")
-    print("Usage: fuzzer.py <Target URL> <Port> <Wordlist>")
+    print("Usage: fuzzer.py <Target URL> <Wordlist>")
     print("")
 
-    if len(sys.argv) !=  4:
+    if len(sys.argv) !=  3:
         print("Not enough arguments")
         exit()
 
     url = sys.argv[1]
-    port = int(sys.argv[2])
-    wordlist = sys.argv[3]
+    wordlist = sys.argv[2]
     error = 0
 
     # Validate the URL
-    isValidURL = validateURL(url)
+    isValidURL = ValidateURL(url)
 
     if isValidURL:
         print("Target: \t" + url)
     else:
         print(colored("Target: \tNot a valid URL or IP address! (%s)" % url, 'red'))
-
-    # Validate the port
-    if port > 65535:
-        print(colored("Port: Not a valid port! (%d)" % port, 'red'))
-        error = 1
-    else:
-        print("Port: \t\t%d" % port)
 
     # Validate that the wordlist exists
     wlExists = FileExists(wordlist)
@@ -71,21 +66,23 @@ if __name__ == "__main__":
 
     if not hasHTTP:
         url = "http://" + url
-
-    # print("Effective URL:\t%s\n" % url)
-
     
-    # # Open the wordlist
+    # Open the wordlist
     wordlistOpen = open(wordlist, 'r')
 
-    print("")
+    print("\nStart fuzzing? (y/n)")
+    confirm = input()
+    
+    if confirm.lower() == "y" or confirm.lower() == "yes":
 
-    for line in wordlistOpen:
-        urlFuzz = url + "/"+ line.replace("\n", "") + "/"
-        r = requests.get(urlFuzz)
-        if r.status_code == 200:
-            print(colored("[*] ", 'blue') + urlFuzz + " " + str(r.status_code))
+        for line in wordlistOpen:
+            urlFuzz = url + "/"+ line.replace("\n", "") + "/"
+            r = requests.get(urlFuzz)
+            if r.status_code == 200:
+                print(colored("[*] ", 'blue') + urlFuzz + " " + str(r.status_code))
 
-    wordlistOpen.close()
+        wordlistOpen.close()
 
 
+    else:
+        print("Exiting...")
